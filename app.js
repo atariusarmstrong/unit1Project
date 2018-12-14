fdb9dcf2adbfd2a13a73d7fe7b48564f999c5a15
Atarius Armstrong <atariusarmstrong@gmail.com>
2018-12-14 13:07:19 -0500

    moved cards into the grid but not wrapping in grid

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //Set cloned items into variables
-var clonedCards = $('.cards:last').clone()
+var clonedCards = $('.cards:last')
 
 
 //Make 30 Divs for playing cards
 for (let i = 0; i < 29; i ++) {
     console.log("cards")
 
-    //clonedCards.clone().appendTo('body')
+    clonedCards.clone().appendTo('.gamingArea')
    
 }
 
 
 //Insert cloned cards to grid
-$(clonedCards).insertAfter('.cards')
+//$(clonedCards).insertAfter('.cards')
