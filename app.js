813e5c0944019f5eda74006a896aa40573d0fb62
Atarius Armstrong <atariusarmstrong@gmail.com>
2018-12-14 18:46:29 -0500

    clickCard alert tester function works

--- app.js
+++ app.js
@@ -20,5 +20,15 @@ for (let i = 0; i < 4; i ++) {
 //Card click tester
 $('.cards').click(function(){
     console.log('This tester works so far.')
+    clickCard()
 })
+
+//When user clicks on card it should open a prompt for a question
+let clickCard = function (){
+    alert("This is an alert in place of the window that should open")
+}
+
+//User will select answer then hit "submit"
+//When user submits answer it should either prompt that it's correct or incorrect
+//When prompt closes, that card is no longer clickable
 
