93b9ea24485ebfaf01faccf99f9889afc684a365
Atarius Armstrong <atariusarmstrong@gmail.com>
2018-12-14 14:23:08 -0500

    finally set up full grid

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 //Set cloned items into variables
-var clonedCards = $('.cards:last')
+var oneCards = $('.one:last')
+var twoCards = $('.two:last')
+var threeCards = $('.three:last')
+var fourCards = $('.four:last')
+var fiveCards = $('.five:last')
 
 
 //Make 30 Divs for playing cards
-for (let i = 0; i < 29; i ++) {
+for (let i = 0; i < 4; i ++) {
     console.log("cards")
 
-    clonedCards.clone().appendTo('.gamingArea')
-   
+    oneCards.clone().appendTo('.gamingArea')
+    twoCards.clone().appendTo('.gamingArea')
+    threeCards.clone().appendTo('.gamingArea')
+    fourCards.clone().appendTo('.gamingArea')
+    fiveCards.clone().appendTo('.gamingArea')
 }
 
 
-//Insert cloned cards to grid
-//$(clonedCards).insertAfter('.cards')
+
