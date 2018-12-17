8af8d1aef3aa81855260f56ac10c3cad9071c4d6
Atarius Armstrong <atariusarmstrong@gmail.com>
2018-12-17 16:40:23 -0500

    got answer options to show up in modal

--- app.js
+++ app.js
@@ -17,28 +17,25 @@ for (let i = 0; i < 4; i ++) {
     fiveCards.clone().appendTo('#cat'+ (i + 2))
 }
 
-//Card click tester now with modal
-// $('.cards').click(function(){
-//     console.log('This tester works so far.') 
-//     clickCard()
-
-// })
-
-//When user clicks on card it should open a prompt for a question
-// let clickCard = function (){
-//     document.getElementById('question').showModal()
-// }
-//let clickCardTwo = function() {
-//    document.getElementById('question2').showModal()
-//}
-
-//User will select answer then hit "submit"
-
-//When user submits answer it should either prompt that it's correct or incorrect
-//When prompt closes, that card is no longer clickable
+//class for question and answer
+class questionModal {
+    constructor(question, options, answer) {
+        this.question = question
+        this.options = options
+        this.answer = answer
+    }
+    //When div is clicked the question and the answer options appear in the modal
+    click(){
+    console.log("clicked" + this.question)
+    $('.questionarea').html(this.question)
+    $('.answerarea').html(this.options)
+    document.getElementById('question').showModal()
+    }
+}
 
 
-//QUESTIONS
+//QUESTIONS COURTESY OF JEOPARDYQUESTIONS.COM
+//CATEGORY ONE QUESTIONS --- MUSIC
 var categoryOne = [{
     question: "This artist is popularly known by another name, but was born as Robyn Fenty",
     options: ["Beyonce", "Rihanna", "Cher", "Mariah Carey"],
@@ -70,19 +67,42 @@ var categoryOne = [{
     answer: 3
 }]
 
-//class for question and answer
-class questionModal {
-    constructor(question, options, answer) {
-        this.question = question
-        this.options = options
-        this.answer = answer
-    }
-    click(){
-    console.log("clicked" + this.question)
-    $('.questionarea').html(this.question)
-    document.getElementById('question').showModal()
-    }
-}
+
+//CATEGORY TWO QUESTIONS -- WILL AND GRACE
+var categoryTwo = [{
+    question: "No big stretch -- he played dad to Willow in I Am Legend & to Jaden in The Pursuit of Happyness",
+    options: ["Will Valderama", "Will Smith", "Will Ferrel", "Will"],
+    answer: 1
+},
+{
+    question: 'In the 1920s this cowboy humorist was honorary mayor of Beverly Hills California',
+    options: ['Andy Warhol', 'Will Rogers', 'William Shakespeare', "Bill Bellamy"],
+    answer: 1
+},
+//NEEDS TO BE UPDATED
+// {
+//     question: 'This elegant dance for couples dominated aristocratic European ballrooms from about 1650 to 1750',
+//     options: ['The Waltz', 'The Minuet', 'The Mirengue', 'The Fox Trot'],
+//     answer: 1
+// },
+// {
+//     question: 'In a Marvin Gaye song this title precedes things aint what they used to be',
+//     options: ['I Want You', 'Lets Get It On', 'Mercy Mercy Me', "You're All I Need to Get By"],
+//     answer: 2
+// },
+// {
+//     question: "In a 1991 top 5 hit Bonnie Raitt sang Let's give them this later the title of a Julia Roberts-Dennis Quaid movie",
+//     options: ['Something To Talk About', 'A Little More Love', 'Holly Jolly Christmas', 'More'],
+//     answer: 0
+// },
+// {
+//     question: 'While the military type of this composition is usually fast and upbeat the funeral type is slow and somber',
+//     options: ['Charleston', 'Gallop', 'Stampede', 'March'],
+//     answer: 3
+// }
+]
+
+
 
 let cardArray = [".one", ".two", ".three", ".four", ".five"]
 
@@ -90,22 +110,12 @@ let musicQuestions = []
 for (let i=0; i<5; i++){
     let musicObject = new questionModal(categoryOne[i].question, categoryOne[i].options, categoryOne[i].answer, (i*100+(100)))
     musicQuestions.push(musicObject)
+    
     console.log($('#cat1 .cards' + cardArray[i]).html())
     $('#cat1 .cards' + cardArray[i]).on('click', ()=> musicQuestions[i].click())
+    
+    
 }
 
 
-//$('#cat1').children().on('click', ()=> musicObject.click())
-
-// $('.cards').click(function(){
-//     console.log('This tester works so far.') 
-//     clickCard()
-
-// })
-
-
-
-//SETTING UP DIALOG WINDOW TESTER FOR QUESTION BOXES
-//Set up dialog in HTML
-//Run function in the script to bring up new window
 
